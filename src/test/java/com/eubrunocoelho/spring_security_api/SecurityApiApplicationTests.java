0a800dedad11561a878d09b4a560a0bca455bf23
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-07-04 17:03:44 -0300

    initial commit

---
+++ src/test/java/com/eubrunocoelho/spring_security_api/SecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eubrunocoelho.spring_security_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
